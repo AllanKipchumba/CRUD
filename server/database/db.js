0f6f90907dbc31b335f543b2dacc0c40ecf7971a
Mohd Mujahed <105550939+Mohd-Mujahed@users.noreply.github.com>
2023-04-21 00:47:34 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 
 const conection = async ()=>{
-    const URL =`mongodb+srv://wildlyelite:k6y6LGPbOu6kONt8@crud-app.7ifnyi6.mongodb.net/?retryWrites=true&w=majority`
+//     enter youre mongodb   url 
+    const URL =`mongodb`
 
     try {
        await  mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });

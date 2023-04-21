9b5e28b55254e76226056af40c2c34f1b5e7c9fe
Mohd Mujahed <105550939+Mohd-Mujahed@users.noreply.github.com>
2023-04-21 07:41:03 +0530

    Delete server directory

--- server/routes/route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import express from "express";
-import { addShop } from "../controller/shop-controller.js";
-
-const router = express.Router();
-
-router.post('/add',addShop)
-
-export default router;
